eed1563a5beec42408155d2f4ae8df9ca2e8d098
chuchu02 <103809166+chuchu02@users.noreply.github.com>
2022-06-10 19:48:16 +0900

    계산기 클래스 리팩토링
    계산기 클래스 리팩토링

--- Calc.java
+++ Calc.java
@@ -3,18 +3,36 @@ package com.exam.exam1;
 public class Calc {
 
     public int run(String s) {
+        s = stripOuterBrackets(s);
 
-        if ( s.charAt(0) == '(' && s.charAt(s.length() - 1) == ')' ) {
-            s= s.substring(1, s.length() - 1);
-        }
-        if (s.indexOf('-') != -1) {
+        String operatorCode = getOperatorCode(s);
+
+        if (operatorCode.equals("-")) {
             return minus(s);
-        } else if (s.indexOf('*') != -1) {
+        } else if (operatorCode.equals("*")) {
             return multi(s);
-        } else if (s.indexOf('/') != -1) {
+        } else if (operatorCode.equals("/")) {
             return devide(s);
+        } else if (operatorCode.equals("/")) {
+            return plus(s);
         }
-        return plus(s);
+        return 0;
+    }
+
+    private String getOperatorCode(String s) {
+        if(s.indexOf('-') != -1) return "-";
+        if(s.indexOf('+') != -1) return "+";
+        if(s.indexOf('*') != -1) return "*";
+        if(s.indexOf('/') != -1) return "/";
+
+        return "unknown";
+    }
+
+    private String stripOuterBrackets(String s) {
+        while (s.charAt(0) == '(' && s.charAt(s.length() - 1) == ')') {
+            s = s.substring(1, s.length() - 1);
+        }
+        return s;
     }
 
     private int devide(String s) {
